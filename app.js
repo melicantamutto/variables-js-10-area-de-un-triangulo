1f42df9825d713b5d3cf2f2ff6ee4a0da9c11f68
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 13:00:00 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Área de un triángulo')
+
+alert('Área de un triángulo')
+
+const base = prompt('Ingresa el valor de la base del triángulo');
+const altura = prompt('Ingresa el valor de la altura del triángulo');
+const area = (base * altura)/2;
+
+alert(`El área del trángulo es de ${area}`);
